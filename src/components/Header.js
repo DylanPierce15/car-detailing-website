890bc6fe955e88dd0124269b7bfce0d35964c2a2
DylanPierce15 <117619443+DylanPierce15@users.noreply.github.com>
2025-05-28 22:44:29 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,8 +10,11 @@ function Header() {
         <div className="header-content">
           <h1 className="main-title"> SHORT HILLS</h1>
           <h1 className="sub-title"> AUTO DETAILING</h1>
-          <h1><br></br></h1>
+          <br></br>
           <h2 className="description"> CAR DETAILING<br />RIGHT ON YOUR<br />DRIVEWAY</h2>
+          <br></br>
+          <br></br>
+          <a href="#booking" className="book-now-button">Book Now</a>
         </div>
       </div>
     </header>
