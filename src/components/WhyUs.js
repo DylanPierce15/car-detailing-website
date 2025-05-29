dcb4c6786176bbbc6fe56e11c48658add06cadff
DylanPierce15 <117619443+DylanPierce15@users.noreply.github.com>
2025-05-29 18:08:25 -0400

    Update WhyUs.js

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -10,9 +10,9 @@ function WhyUs(){
             <div className="reasons-grid">
                 <div className="reason-card">
                     <h3>1. UNMATCHED <br></br> CONVENIENCE</h3>
-                    <h4>Say goodbye to the hassle of driving to a car wash and waiting in line. With out mobile detailing service,
-                        wearing the car wash to you. Our team will arrive promptly  to take care of your vehicle while 
-                        you carry on with  your day hassle-free.</h4>
+                    <h4>Say goodbye to the hassle of driving to a car wash and waiting in line. With our mobile detailing service,
+                        we bring the car wash to you. Our team will arrive promptly to take care of your vehicle while 
+                        you carry on with your day hassle-free.</h4>
                 </div>
                 <div className="reason-card">
                     <h3>2. PERSONALIZED <br></br> ATTENTION</h3>
@@ -25,12 +25,12 @@ function WhyUs(){
                     <h3>3. SUPERIOR <br></br> QUALITY</h3>
                     <h4>At Short Hills Auto Detailing, we take pride in delivering exceptional results with a keen eye for detail. Unlike traditional car washes with time 
                         constraints, our mobile detailing professionals take the time to thoroughly clean and detail your vehicle, leaving no corner untouched, 
-                        We utilize top-of-the -line products and equipment to ensure a pristine finish that exceeds your expectations.</h4>
+                        We utilize top-of-the-line products and equipment to ensure a pristine finish that exceeds your expectations.</h4>
                 </div>
                 <div className="reason-card">
                     <h3> 4. ENHANCED RESALE <br></br> VALUE</h3>
-                    <h4>Did you know that regular car  detailing can actually improve your vehicle’s value? By maintaining your car’s 
-                    exterior and interior in optimal  condition, you’re preserving its appearance and protecting it from 
+                    <h4>Did you know that regular car detailing can actually improve your vehicle’s value? By maintaining your car’s 
+                    exterior and interior in optimal condition, you’re preserving its appearance and protecting it from 
                     wear and tear. Whether you’re planning to sell or trade your vehicle in the  future, investing in regular 
                     detailing can help maintain its resale value and ensure you get the best possible return on your investment.</h4>
                 </div>
